0da688c06f50bf1cf5afcc1c8424c712f0f76a73
Gustavo Cabral <studiogustavocabral@gmail.com>
2022-10-15 07:26:18 -0300

    Added

---
+++ app.js
@@ -0,0 +1,49 @@
+
+
+
+const { ApolloServer, gql } = require ('apollo-server')
+
+const books = [
+  {
+    titulo: 'The Awakening',
+    autor: 'Kate Chopin',
+  },
+
+  {
+    titulo: 'City of Glass',
+    autor: 'Paul Auster',
+  },
+];
+
+
+
+const typeDefs = gql`
+    type Livro {
+        titulo: String
+        autor: String
+    }
+
+    type Query {
+        livros: [Livro],
+        hello: String
+        
+    }
+`
+
+    const resolvers = {
+        Query: {
+          livros: () => books,
+        //   livros(){
+        //     return books
+        //   },
+          hello: () => 'World!!'
+        },
+    };
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+
+server.listen()
--- index.js
+++ index.js
@@ -1,40 +1,72 @@
 const { ApolloServer, gql } = require ('apollo-server')
 
-const books = [
-  {
-    titulo: 'The Awakening',
-    autor: 'Kate Chopin',
-  },
+    const usuariosLista = [
+      {id:1, nome: 'Gustavo'},
+      {id:2, nome: 'Luciane'},
+      {id:3, nome: 'Luana'}
 
-  {
-    titulo: 'City of Glass',
-    autor: 'Paul Auster',
-  },
-];
+    ]
 
 
+    const produtosLista = [
+      {
+        id: 1,
+        nome: 'Macbook',
+        preco: 5000
+      },
+      {
+        id:2,
+        nome: 'iMac',
+        preco: 9000
+      }
 
-const typeDefs = gql`
-    type Livro {
-        titulo: String
-        autor: String
-    }
+    ]
 
-    type Query {
-        livros: [Livro],
-        hello: String
+    const typeDefs = gql`
+
+        type Usuario {
+          id:ID
+          nome: String
+        }
+        type Produto {
+          id:ID
+          nome:String
+          preco: Int
+        }
+
         
-    }
-`
+        type Query {
+            usuarios: [Usuario]
+            produtos: [Produto]
+            usuario(id: Int, nome:String): Usuario
+            produto(id: Int, nome:String):Produto
+        }
+    `
 
     const resolvers = {
-        Query: {
-          livros: () => books,
-        //   livros(){
-        //     return books
-        //   },
-          hello: () => 'World!!'
+      // Cada query tem que ser denominada com uma função
+      Query: {
+        usuarios(){
+          return usuariosLista
         },
+
+        produtos(){
+          return produtosLista
+        },
+
+        usuario(_, args){
+          const {id, nome} = args;
+          if (id) return usuariosLista.find(usuario => usuario.id === id)
+          return usuariosLista.find(usuario => usuario.nome === nome)
+        },
+
+        produto(_,args){
+          const {id} = args
+          return produtosLista.find(produto => produto.id === id)
+        }
+
+           }
+      
     };
 
 const server = new ApolloServer({
